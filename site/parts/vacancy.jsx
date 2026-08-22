c8bec0d2fdd3b6f3a2ae6e997759a44e7c16a444
Saeed Nemati <sn.1361@gmail.com>
2026-08-22 19:15:34 +0330

    committed by script

--- site/parts/vacancy.jsx
+++ site/parts/vacancy.jsx
@@ -1,4 +1,4 @@
-export default ({ vacancy }) => <article class='vacancyCard'>
+export default ({ vacancy }) => <article class='vacancy'>
     <h2 class='title'>{vacancy.title}</h2>
     <time class='closingDate'>{vacancy.closingDate}</time>
     <div class='description'>{vacancy.description}</div>
